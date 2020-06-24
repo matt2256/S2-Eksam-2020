47140bcd377b8188508f6c81d962fac7a78429f4
matt2256 <mads@mmm-m.dk>
2020-06-24 13:39:17 +0200

    Added a simple test case to the solution, to test if the pai key return is same as input

---
+++ HTK - Solution/SimpleTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API;
+
+namespace SimpleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestApiKey()
+        {
+            Control _control = new Control();
+            string test = _control.ApiKey();
+            Assert.AreEqual(test, "291c93497e96ae5f8e2b01e60a34a1c1");
+        }
+    }
+}
