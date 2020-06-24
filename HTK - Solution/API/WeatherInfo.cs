a198f79cf711655df7b676f0e755f85ceecebdbf
matt2256 <mads@mmm-m.dk>
2020-06-24 10:34:20 +0200

    Added cummary comments to WeatherInfo

--- HTK - Solution/API/WeatherInfo.cs
+++ HTK - Solution/API/WeatherInfo.cs
@@ -9,18 +9,27 @@ namespace API
 {
     class WeatherInfo
     {
+        /// <summary>
+        /// Represent a class called coord to temperorary store coord data, gotten from OpenWeatherApi current weather api call
+        /// </summary>
         public class coord
         {
             public double Ion { get; set; }
             public double lat { get; set; }
         }
-
+        
+        /// <summary>
+        /// Represent a class called weather to temperoary store weather data, gotten from OpenWeatherApi current weather api call
+        /// </summary>
         public class weather
         {
             public string main { get; set; }
             public string description { get; set; }
         }
 
+        /// <summary>
+        /// Represent a class callled main to temperorary store main data, gotten from OpenWeatherApi current weather api call
+        /// </summary>
         public class main
         {
             public double temp { get; set; }
@@ -28,16 +37,25 @@ namespace API
             public double humidity { get; set; }
         }
 
+        /// <summary>
+        /// Represent a class called wind to temperorary store wind data, gotten from OpenWeatherApi current weather api call
+        /// </summary>
         public class wind
         {
             public double speed { get; set; }
         }
 
+        /// <summary>
+        /// Represent a class callled sys to temporary store sys data, gotten from OpenWeatherApi current weather api call
+        /// </summary>
         public class sys
         {
             public string country { get; set; }
         }
 
+        /// <summary>
+        /// Represent a called root to collect alle data gotten from different classes to make it easier to control
+        /// </summary>
         public class root
         {
             public string name { get; set; }
